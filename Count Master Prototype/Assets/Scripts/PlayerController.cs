3d9bd6673e83d04707e3a5e96224260365b5f194
Tolga Sen <tolgasen9558@gmail.com>
2023-09-30 21:36:17 +0200

    Added Character and Controller

---
+++ Count Master Prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float sensitivity;
+
+    private float maxOffset = 3.5f;
+    private float horizontalInput;
+    private Vector3 finalPos;
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * sensitivity * Time.deltaTime);
+        finalPos = transform.position;
+
+        finalPos.x = Mathf.Min(maxOffset, finalPos.x);
+        finalPos.x = Mathf.Max(-maxOffset, finalPos.x);
+        transform.position = finalPos;
+    }
+}
