ed2a2e3019f528b85e8afab0c8fe5d39fabe95b4
Tolga Sen <tolgasen9558@gmail.com>
2023-10-28 12:52:56 +0200

    Door Editor problems fixed

--- Count Master Prototype/Assets/Editor/DoorAffectEditorScript.cs
+++ Count Master Prototype/Assets/Editor/DoorAffectEditorScript.cs
@@ -2,51 +2,29 @@ using UnityEngine;
 using UnityEditor;
 using Unity.VisualScripting;
 using TMPro;
-using UnityEditor.SceneManagement;
 
 [CustomEditor(typeof(Door))]
 public class DoorAffectEditorScript : Editor
 {
 
-    int slideValue;
-    private Door door;
+    SerializedProperty affectValue;
     private TMP_Text affectText;
 
     private void OnEnable()
     {
-        door = target.GetComponent<Door>();
-        affectText = findTMPComponent(door);
-        slideValue = door.GetAffectCount();
+        affectValue = serializedObject.FindProperty("affectCount");
+        affectText = findTMPComponent(target);
     }
 
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
-        EditorGUILayout.BeginHorizontal();
-        EditorGUILayout.LabelField("Affect Value", EditorStyles.boldLabel);
-        slideValue = EditorGUILayout.IntSlider(slideValue, -100, 100);
-        affectText.text = addPlusSignIfPositive(slideValue);
-        door.SetAffectCount(slideValue);
-        EditorGUILayout.EndHorizontal();
+        serializedObject.Update();
+        EditorGUILayout.IntSlider(affectValue, -100, 100);
+        affectText.text = addPlusSignIfPositive(affectValue.intValue);
+        EditorUtility.SetDirty(affectText);
+        serializedObject.ApplyModifiedProperties();
 
-        if (GUI.changed)
-        {
-            EditorUtility.SetDirty(door);
-            EditorSceneManager.MarkSceneDirty(door.gameObject.scene);
-        }
-
-    }
-
-    private void OnValidate()
-    {
-        affectText.text = addPlusSignIfPositive(slideValue);
-        Debug.Log("Validate called");
-    }
-
-    private void OnDisable()
-    {
-        affectText.text = addPlusSignIfPositive(slideValue);
-        Debug.Log("Disable called");
     }
 
     private string addPlusSignIfPositive(int value)
@@ -56,14 +34,14 @@ public class DoorAffectEditorScript : Editor
         return value.ToString() ; 
     }
 
-    private TMP_Text findTMPComponent(Door door)
+    private TMP_Text findTMPComponent(Object parent)
     {
         // Find the canvas first
         var childCanvas= target.GetComponentInChildren<Canvas>();
         if (childCanvas == null)
         {
             Debug.LogWarning("Can't find canvas in the children of " 
-                + door.gameObject + ". Check if there is a canvas object attached to it");
+                + parent + ". Check if there is a canvas object attached to it");
             return null;
         }
 
@@ -71,8 +49,8 @@ public class DoorAffectEditorScript : Editor
         var tmpText = childCanvas.GetComponentInChildren<TMP_Text>();
         if (tmpText == null)
         {
-            Debug.LogWarning("Can't find canvas in the children of "
-                + door.gameObject + ". Check if there is a canvas object attached to it");
+            Debug.LogWarning("Can't find TextMeshPro in the children of "
+                + parent + ". Check if there is a canvas object attached to it");
             return null;
         }
 
--- Count Master Prototype/Assets/Scripts/Door.cs
+++ Count Master Prototype/Assets/Scripts/Door.cs
@@ -2,37 +2,26 @@ using TMPro;
 using UnityEngine;
 
 
-[System.Serializable]
 public class Door : MonoBehaviour
 {
     [SerializeField, HideInInspector]
     private int affectCount;
 
-    private bool active = true;
+    [SerializeField, HideInInspector]
     private TMP_Text affectText;
 
-
-    private void Awake()
-    {
-        affectText = findTMPComponent(this);
-    }
-
-    private void Start()
-    {
-        affectText.text = addPlusSignIfPositive(affectCount);  
-    }
-
+    private bool isActive = true;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!active)
+        if (!isActive)
             return;
 
         PlayerController playerController = ScriptableObject.CreateInstance<PlayerController>();    
         var player = other.GetComponent<Player>();
         if (player)
         {
-            active = false;
+            isActive = false;
 
             Debug.Log(gameObject.name + " collided with player");
             if(affectCount < 0 )
@@ -46,45 +35,5 @@ public class Door : MonoBehaviour
                 playerController.GrowMinionCount(affectCount);
             }
         }
-    }
-
-    private string addPlusSignIfPositive(int value)
-    {
-        if (value > 0) return "+" + value;
-
-        return value.ToString();
-    }
-
-    public void SetAffectCount(int affectCount)
-    {
-        this.affectCount = affectCount;
-    }
-
-    public int GetAffectCount()
-    {
-        return this.affectCount;
-    }
-
-    private TMP_Text findTMPComponent(Door door)
-    {
-        // Find the canvas first
-        var childCanvas = GetComponentInChildren<Canvas>();
-        if (childCanvas == null)
-        {
-            Debug.LogWarning("Can't find canvas in the children of "
-                + door.gameObject + ". Check if there is a canvas object attached to it");
-            return null;
-        }
-
-        // Find the TextMeshPro component
-        var tmpText = childCanvas.GetComponentInChildren<TMP_Text>();
-        if (tmpText == null)
-        {
-            Debug.LogWarning("Can't find canvas in the children of "
-                + door.gameObject + ". Check if there is a canvas object attached to it");
-            return null;
-        }
-
-        return tmpText;
     }
 }
