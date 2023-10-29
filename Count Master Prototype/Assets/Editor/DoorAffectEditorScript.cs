e3661063b93d8c0dfb6def3aaee0b9486dc7df97
Tolga Sen <tolgasen9558@gmail.com>
2023-10-29 14:39:22 +0100

    Added functionality to change door colors based on the affectValue

--- Count Master Prototype/Assets/Editor/DoorAffectEditorScript.cs
+++ Count Master Prototype/Assets/Editor/DoorAffectEditorScript.cs
@@ -10,21 +10,62 @@ public class DoorAffectEditorScript : Editor
     SerializedProperty affectValue;
     private TMP_Text affectText;
 
+    //Door colors
+    private Material notWorkingDoorMaterial;
+    private Material plusDoorMaterial;
+    private Material minusDoorMaterial;
+    private Renderer renderer;
+
     private void OnEnable()
     {
         affectValue = serializedObject.FindProperty("affectCount");
         affectText = findTMPComponent(target);
+        loadMaterials();
+        renderer = target.GetComponent<Renderer>();
     }
 
     public override void OnInspectorGUI()
     {
+        // Needs to be called before any modifications
         base.OnInspectorGUI();
         serializedObject.Update();
+
+        // Create and use the value of the slider
         EditorGUILayout.IntSlider(affectValue, -100, 100);
-        affectText.text = addPlusSignIfPositive(affectValue.intValue);
+
+        handleDoorText();
+        handleDoorColor(affectValue.intValue);
+        saveChanges();
+    }
+
+    private void saveChanges()
+    {
         EditorUtility.SetDirty(affectText);
         serializedObject.ApplyModifiedProperties();
+    }
 
+    private void handleDoorText()
+    {
+        affectText.text = addPlusSignIfPositive(affectValue.intValue);
+    }
+
+    // Changes the color based on the affectValue
+    private void handleDoorColor(int affectValue)
+    {
+        Material materialToApply;
+        if (affectValue > 0)
+        {
+            materialToApply = plusDoorMaterial;
+        }
+        else if (affectValue == 0)
+        {
+            materialToApply = notWorkingDoorMaterial;
+        }
+        else
+        {
+            materialToApply = minusDoorMaterial;
+        }
+        renderer.sharedMaterial = materialToApply;
     }
 
     private string addPlusSignIfPositive(int value)
@@ -32,6 +73,13 @@ public class DoorAffectEditorScript : Editor
         if(value > 0) return "+" + value ;
 
         return value.ToString() ; 
+    }
+
+    private void loadMaterials()
+    {
+        notWorkingDoorMaterial = Resources.Load("Material/NotWorkingDoor", typeof(Material)) as Material;
+        plusDoorMaterial = Resources.Load("Material/PlusDoor", typeof(Material)) as Material;
+        minusDoorMaterial = Resources.Load("Material/MinusDoor", typeof(Material)) as Material;
     }
 
     private TMP_Text findTMPComponent(Object parent)
